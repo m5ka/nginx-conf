aa1b009266cfd52ce40849fc39c5ca80f1f39759
Maria <25516941+m5ka@users.noreply.github.com>
2023-12-04 20:53:44 +0000

    🐍 python certificate script and more sites

---
+++ prepare.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+import tomllib
+from os.path import exists
+
+
+if __name__ == "__main__":
+    with open("certificates.toml") as file:
+        try:
+            toml = tomllib.load(file)
+        except tomllib.TOMLDecodeError:
+            print("Could not decode certificates.toml", file=sys.stderr)
+            sys.exit(1)
+
+        for name, domains in toml.items():
+            if exists(f"/etc/letsencrypt/live/{name}/fullchain.pem") and exists(
+                "/etc/letsencrypt/live/{name}/privkey.pem"
+            ):
+                print("✅ {name} already exists!")
+            else:
+                d_str = " ".join([f"-d {d}" for d in domains])
+                ret = subprocess.call(
+                    f"certbot certonly --cert-name {name} "
+                    f"--webroot -w /var/www/html {d_str}"
+                )
+                if ret == 0:
+                    print(f"✅ {name} is done!")
+                else:
+                    print(
+                        f"😥 Something went wrong requesting a certificate for {name}.",
+                        file=sys.stderr,
+                    )
+                    sys.exit(1)
